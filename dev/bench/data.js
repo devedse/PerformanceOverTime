8266709ae636c8574bd1fff864531659715a913d
github-action-benchmark <github@users.noreply.github.com>
2022-03-11 12:56:16 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 2a5c4a860b25704dcb9e22207d8c3f2f65c5c2df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647003017570,
+  "lastUpdate": 1647003375988,
   "repoUrl": "https://github.com/devedse/PerformanceOverTime",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 320360498.25,
             "unit": "ns",
             "range": "± 1265888.3415358507"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davydavidse@gmail.com",
+            "name": "Devedse",
+            "username": "devedse"
+          },
+          "committer": {
+            "email": "davydavidse@gmail.com",
+            "name": "Devedse",
+            "username": "devedse"
+          },
+          "distinct": true,
+          "id": "2a5c4a860b25704dcb9e22207d8c3f2f65c5c2df",
+          "message": "Test checkin",
+          "timestamp": "2022-03-11T13:54:45+01:00",
+          "tree_id": "7d04362470dbc03dfd706fb7c040c287e87f1307",
+          "url": "https://github.com/devedse/PerformanceOverTime/commit/2a5c4a860b25704dcb9e22207d8c3f2f65c5c2df"
+        },
+        "date": 1647003375081,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "MazeGeneratorPerformanceTest.BenchmarkJob.AlgorithmBacktrack2",
+            "value": 363836525.2307692,
+            "unit": "ns",
+            "range": "± 4041018.1453667334"
+          },
+          {
+            "name": "MazeGeneratorPerformanceTest.BenchmarkJob.AlgorithmBacktrack2Deluxe2",
+            "value": 332997411.31578946,
+            "unit": "ns",
+            "range": "± 6609508.44382365"
           }
         ]
       }
