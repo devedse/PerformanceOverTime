ec352e6171f3f6614bbce3c4f7e72d3eab6ad78a
Devedse <davydavidse@gmail.com>
2022-03-11 10:41:02 +0100

    Added application

---
+++ MazeGeneratorPerformanceTest/MazeGeneratorPerformanceTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using DeveMazeGeneratorCore.Factories;
+using DeveMazeGeneratorCore.Generators;
+using DeveMazeGeneratorCore.Generators.Helpers;
+using DeveMazeGeneratorCore.Generators.SpeedOptimization;
+using DeveMazeGeneratorCore.Imageification;
+using DeveMazeGeneratorCore.InnerMaps;
+using DeveMazeGeneratorCore.PathFinders;
+using System.Diagnostics;
+
+namespace MazeGeneratorPerformanceTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating maze...");
+
+            int size = 16384;
+            var fastestElapsed = TimeSpan.MaxValue;
+
+            var alg = new AlgorithmBacktrack2Deluxe2();
+
+            Console.WriteLine($"Generating mazes using {alg.GetType().Name}...");
+
+            int seed = 1337;
+
+
+            var innerMapFactory = new InnerMapFactory<BitArreintjeFastInnerMap>(size, size);
+            var randomFactory = new RandomFactory<XorShiftRandom>(seed);
+
+            var wMaze = Stopwatch.StartNew();
+            var maze = alg.GoGenerate2(innerMapFactory, randomFactory, new NoAction());
+            wMaze.Stop();
+
+            Console.WriteLine($"Maze generation time: {wMaze.Elapsed}");
+
+            var wPath = Stopwatch.StartNew();
+            var path = PathFinderDepthFirstSmartWithPos.GoFind(maze, null);
+            wPath.Stop();
+
+            Console.WriteLine($"Maze pathfinding time: {wMaze.Elapsed}");
+
+            var wSave = Stopwatch.StartNew();
+            using (var fs = new FileStream($"icon{seed}.png", FileMode.Create))
+            {
+                WithPath.SaveMazeAsImageDeluxePng(maze, path, fs);
+            }
+            wSave.Stop();
+
+            Console.WriteLine($"Maze save to image time: {wMaze.Elapsed}");
+        }
+    }
+}
